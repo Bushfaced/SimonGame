dfbe435b9ad9c489b4aceedf3e1a97d72e9197c5
richardguzman <thisisrichardguzman@gmail.com>
2022-04-01 16:58:06 -0700

    Initial commit

---
+++ js folder/main.js
@@ -0,0 +1,26 @@
+//*********/ CONSTANTS *****************/
+
+//*********/ STATE VARIABLES ***********/
+let rounds;
+let intervals;
+let loseCondition;
+let playerChoices;
+let compTurn;
+//*********/ Event Listeners ***********/
+
+
+renderCompPattern;
+
+document.querySelector('main').addEventListener('click', handleButton)
+
+// ******function********* //
+
+function init() {
+    
+};
+
+
+
+function handleButton() {
+
+}
